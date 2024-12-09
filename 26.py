8155f094c3dd6ed93307e243d2ca94b99b7fb011
Akshay_Aneesh <120236337+Akshaysonu916@users.noreply.github.com>
2024-12-09 19:55:41 +0530

    second commit

---
+++ 26.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self):
+        self.age=20
+p1=Person()
+p1.age=21
+print(p1.age)
---
+++ new.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+sum=0
+for n in range(a,b+1):
+  sum=sum+n
+print(sum)
